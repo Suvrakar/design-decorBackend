be02a79410d784ae32c91d3fa9fe7b9ffcd300b7
Suvra <kar.suvra@gmail.com>
2021-12-22 16:12:17 +0600

    res solved

--- controller.js
+++ controller.js
@@ -58,7 +58,7 @@ module.exports = class ContactController {
                 }
                 return res(result)
             });
-            res(con)
+            return res([])
         });
 
     }
